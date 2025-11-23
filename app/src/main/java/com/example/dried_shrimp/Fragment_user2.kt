691bf15db88d890fcae2339414f9b74f0220ab25
a3137418 <a3137418@gmail.com>
2025-11-23 16:07:59 +0800

    修改登入及註冊頁面密碼輸入隱藏及開眼功能

--- app/src/main/java/com/example/dried_shrimp/Fragment_user2.kt
+++ app/src/main/java/com/example/dried_shrimp/Fragment_user2.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,7 +14,6 @@ import com.google.firebase.auth.FirebaseAuth
 
 class Fragment_user2: Fragment(){
     private var binding: FragmentUser2Binding ?= null
-    private lateinit var userContainer: FrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
